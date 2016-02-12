65860da8d0f149ad86e7dbd337059467fdbe7d9d
Vitaly Kondratiev <vitalkond@gmail.com>
2016-02-12 20:54:03 +0000

    adds tests for Utils

--- tests/utils/index-test.js
+++ tests/utils/index-test.js
@@ -1,24 +1,38 @@
 import React, { createElement as $ } from 'react';
 import { hasChild } from 'src/utils';
-import { Button as Element, Icon } from 'src/';
+import { Button as Element, Icon, Label } from 'src/';
 import { expect } from 'chai';
 import sd from '../../test-helpers/skin-deep';
 
 let props = {};
 
 describe('Utils', () => {
-    describe('should return correct values for hasChild', () => {
+    describe('hasChild should return correct values', () => {
         beforeEach(() => {
             props = {
-                name: 'icon'
+                name: 'icon',
+                'aria-label': 'icon button'
             };
         });
 
-        it('returns true if child exists', () => {
+        it('returns true if the specified child exists', () => {
             const tree = sd.shallowRender($(Element, props, <Icon name="glyphicon" />));
             const vdom = tree.getRenderOutput();
 
-            console.log(hasChild(tree));
+            expect(hasChild(vdom.props.children, Icon)).to.equal(true);
+        });
+        it('returns false if the specified child does not exist', () => {
+            let tree = sd.shallowRender(
+                $(Element, props, <Label name="username" htmlFor="username">Username</Label>)
+            );
+            let vdom = tree.getRenderOutput();
+
+            expect(hasChild(vdom.props.children, Icon)).to.equal(false);
+
+            tree = sd.shallowRender($(Element, props));
+            vdom = tree.getRenderOutput();
+
+            expect(hasChild(vdom.props.children, Icon)).to.equal(false);
         });
     });
 });
